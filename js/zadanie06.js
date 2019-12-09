31b4bd92790c0f296d77a576c05e29f0b6783934
marcinbucwinski <43888277+marcinbucwinski@users.noreply.github.com>
2019-12-09 23:59:37 +0100

    Update zadanie06.js

--- js/zadanie06.js
+++ js/zadanie06.js
@@ -1,22 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-   
-   var button1=document.getElementById("button-1");
+    var button1=document.getElementById("button-1");
 button1.addEventListener("click", function addItem() {
-    var a=document.createElement("li");
-    a.innerHTML="Chleb";
-    document.getElementById("shopping-list").appendChild(a);
+    var createOfElement=document.createElement("li");
+    createOfElement.innerHTML="Chleb";
+    document.getElementById("shopping-list").appendChild(createOfElement);
 });
 
 var button2=document.getElementById("button-2");
 button2.addEventListener("click", function removeItem() {
-    var b=document.getElementById("shopping-list");
-    b.removeChild(b.lastElementChild);    
+    var removeElement=document.getElementById("shopping-list");
+    removeElement.removeChild(removeElement.lastElementChild);    
 });
 
 var button3=document.getElementById("button-3");
 button3.addEventListener("click", function cloneItem() {
-    var c=document.getElementById("shopping-list").children[1];
-    var d=c.cloneNode(true);
-    document.getElementById("shopping-list").appendChild(d);    
+    var gettingElement=document.getElementById("shopping-list").children[1];
+    var cloneElement=gettingElement.cloneNode(true);
+    document.getElementById("shopping-list").appendChild(cloneElement);    
 });
 });
